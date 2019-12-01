51ef3913a30f7cc53e925e0825dc4656a1b4759b
Florian F <53172803+FDC4000@users.noreply.github.com>
2019-12-01 02:36:05 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mv ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh.orig
+cp cpu_temp.sh ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh
+cp is_pi_plattform.sh ~/.tmux/plugins/tmux-cpu/scripts/
