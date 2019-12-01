f540156f489d4d614739c735decae9e2e3d3ab01
FDC4000 <53172803+FDC4000@users.noreply.github.com>
2019-12-01 03:01:18 +0100

    Enhance install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-mv ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh.orig
-cp cpu_temp.sh ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh
-cp is_pi_plattform.sh ~/.tmux/plugins/tmux-cpu/scripts/
-chmod +x ~/.tmux/plugins/tmux-cpu/scripts/cpu_temp.sh
+startPlace='${pwd}'
+cd ~/.tmux/plugins/tmux-cpu/scripts/
+mv cpu_temp.sh cpu_temp.sh.orig
+cp $startPlace/cpu_temp.sh ./cpu_temp.sh
+chmod +x cpu_temp.sh
+cp $startPlace/is_pi_plattform.sh ./is_pi_plattform.sh
+chmod +x is_pi_plattform.sh
